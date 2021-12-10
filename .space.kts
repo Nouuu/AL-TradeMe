83fb393f15db82fb58a354e0121014e1151e1095
Noe Larrieu-Lacoste <nlarrieulacoste@myges.fr>
2021-12-10 16:45:50 +0000

    change action trigger

--- .space.kts
+++ .space.kts
@@ -1,5 +1,13 @@
 job("Run tests") {
-   container("maven:3-openjdk-11") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/main"
+                +Regex("feature")
+            }
+        }
+    }
+   container(displayName = "Run mvn test", "maven:3-openjdk-11") {
        shellScript {
            content = "mvn test"
        }
