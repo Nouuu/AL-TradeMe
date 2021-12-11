0077d952e967995a5046fd1213878f819adbe0e2
Nouu <noelarrieulacoste@yahoo.fr>
2021-12-11 16:24:17 +0100

    logger getSimpleName for class

--- src/main/java/org/larrieulacoste/noe/al/trademe/infrastructure/logger/DefaultLoggerFactory.java
+++ src/main/java/org/larrieulacoste/noe/al/trademe/infrastructure/logger/DefaultLoggerFactory.java
@@ -9,6 +9,6 @@ public final class DefaultLoggerFactory implements LoggerFactory {
 
     @Override
     public Logger getLogger(Object object) {
-        return new DefaultLogger(Objects.requireNonNull(object).getClass().getName());
+        return new DefaultLogger(Objects.requireNonNull(object).getClass().getSimpleName());
     }
 }
