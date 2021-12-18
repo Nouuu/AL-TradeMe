77e807264b81c0a5aac1235dd0419910f829464d
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-12-18 18:19:57 +0100

    make entity props final

--- src/main/java/org/larrieulacoste/noe/al/trademe/features/members/domain/Tradesman.java
+++ src/main/java/org/larrieulacoste/noe/al/trademe/features/members/domain/Tradesman.java
@@ -9,10 +9,10 @@ import java.util.Objects;
 
 public final class Tradesman {
     private final EntityId entityId;
-    private NotEmptyString lastname;
-    private NotEmptyString firstname;
-    private EmailAddress email;
-    private Password password;
+    private final NotEmptyString lastname;
+    private final NotEmptyString firstname;
+    private final EmailAddress email;
+    private final Password password;
 
     private Tradesman(EntityId entityId, NotEmptyString lastname, NotEmptyString firstname, EmailAddress email, Password password) {
         this.entityId = Objects.requireNonNull(entityId);
