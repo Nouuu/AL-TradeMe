05a3f7da1c6ef96f3b99bbea7e0391c432a49293
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-12-27 16:15:57 +0100

    DefaultLoggerTest

---
+++ src/test/java/org/larrieulacoste/noe/al/trademe/infrastructure/logger/DefaultLoggerTest.java
@@ -0,0 +1,18 @@
+package org.larrieulacoste.noe.al.trademe.infrastructure.logger;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DefaultLoggerTest {
+    private final DefaultLogger defaultLogger = new DefaultLogger("Logger");
+
+    @Test()
+    void log() {
+        Assertions.assertAll(() -> defaultLogger.log("log message"));
+    }
+
+    @Test
+    void error() {
+        Assertions.assertAll(() -> defaultLogger.error("error message"));
+    }
+}
