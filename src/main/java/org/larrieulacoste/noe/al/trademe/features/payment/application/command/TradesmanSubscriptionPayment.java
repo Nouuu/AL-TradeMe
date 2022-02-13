9e2cf63ff3c032fea2d820c279702057b0fb875c
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-13 18:37:35 +0100

    payment value objects

--- src/main/java/org/larrieulacoste/noe/al/trademe/features/payment/application/command/TradesmanSubscriptionPayment.java
+++ src/main/java/org/larrieulacoste/noe/al/trademe/features/payment/application/command/TradesmanSubscriptionPayment.java
@@ -4,35 +4,8 @@ import org.larrieulacoste.noe.al.trademe.domain.model.EntityId;
 import org.larrieulacoste.noe.al.trademe.domain.model.PaymentMethod;
 import org.larrieulacoste.noe.al.trademe.kernel.command.Command;
 
-import java.util.Objects;
-
-public final class TradesmanSubscriptionPayment implements Command {
-    public final EntityId tradesmanId;
-    public final PaymentMethod paymentMethod;
-
-    public TradesmanSubscriptionPayment(EntityId tradesmanId, PaymentMethod paymentMethod) {
-        this.tradesmanId = tradesmanId;
-        this.paymentMethod = paymentMethod;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TradesmanSubscriptionPayment that = (TradesmanSubscriptionPayment) o;
-        return Objects.equals(tradesmanId, that.tradesmanId) && Objects.equals(paymentMethod, that.paymentMethod);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(tradesmanId, paymentMethod);
-    }
-
-    @Override
-    public String toString() {
-        return "TradesmanPayment{" +
-                "tradesmanId=" + tradesmanId +
-                ", paymentMethod='" + paymentMethod + '\'' +
-                '}';
-    }
+public record TradesmanSubscriptionPayment(
+        EntityId tradesmanId,
+        PaymentMethod paymentMethod
+) implements Command {
 }
