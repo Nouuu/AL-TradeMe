207c365d02b06b83acb384b0c7d8105bdf310b67
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-13 14:47:08 +0100

    revise querybus configuration

--- src/main/java/org/larrieulacoste/noe/al/trademe/configuration/QueryConfiguration.java
+++ src/main/java/org/larrieulacoste/noe/al/trademe/configuration/QueryConfiguration.java
@@ -2,13 +2,14 @@ package org.larrieulacoste.noe.al.trademe.configuration;
 
 import org.larrieulacoste.noe.al.trademe.features.invoices.application.query.*;
 import org.larrieulacoste.noe.al.trademe.features.invoices.kernel.DefaultInvoicesQueryBus;
-import org.larrieulacoste.noe.al.trademe.features.invoices.kernel.InvoicesQueryBus;
 import org.larrieulacoste.noe.al.trademe.features.members.application.query.*;
 import org.larrieulacoste.noe.al.trademe.features.members.kernel.DefaultMembersQueryBus;
 import org.larrieulacoste.noe.al.trademe.features.members.kernel.MembersQueryBus;
 import org.larrieulacoste.noe.al.trademe.features.payment.kernel.DefaultPaymentQueryBus;
 import org.larrieulacoste.noe.al.trademe.features.payment.kernel.PaymentQueryBus;
+import org.larrieulacoste.noe.al.trademe.kernel.query.DefaultQueryBus;
 import org.larrieulacoste.noe.al.trademe.kernel.query.Query;
+import org.larrieulacoste.noe.al.trademe.kernel.query.QueryBus;
 import org.larrieulacoste.noe.al.trademe.kernel.query.QueryHandler;
 
 import javax.enterprise.context.Dependent;
@@ -44,9 +45,10 @@ final class QueryConfiguration {
 
     @Produces
     @Singleton
-    InvoicesQueryBus invoicesQueryBus() {
+    QueryBus queryBus() {
         Map<Class<? extends Query>, QueryHandler<? extends Query, ?>> queryMap = new HashMap<>();
 
+        // Invoices
         queryMap.put(RetrieveAllInvoices.class, retrieveAllInvoicesService);
         queryMap.put(RetrieveContractorsInvoices.class, retrieveContractorsInvoicesService);
         queryMap.put(RetrieveContractorInvoices.class, retrieveContractorInvoicesService);
@@ -54,28 +56,14 @@ final class QueryConfiguration {
         queryMap.put(RetrieveTradesmanInvoices.class, retrieveTradesmanInvoicesService);
         queryMap.put(RetrieveInvoiceById.class, retrieveInvoiceByIdService);
 
-        return new DefaultInvoicesQueryBus(queryMap);
-    }
-
-    @Produces
-    @Singleton
-    MembersQueryBus membersQueryBus() {
-        Map<Class<? extends Query>, QueryHandler<? extends Query, ?>> queryMap = new HashMap<>();
-
+        // Members
         queryMap.put(RetrieveContractorById.class, retrieveContractorByIdService);
         queryMap.put(RetrieveContractors.class, retrieveContractorsService);
         queryMap.put(RetrieveTradesmanById.class, retrieveTradesmanByIdService);
         queryMap.put(RetrieveTradesmen.class, retrieveTradesmenService);
 
-        return new DefaultMembersQueryBus(queryMap);
+        // Payments
+
+        return new DefaultQueryBus(queryMap);
     }
-
-    @Produces
-    @Singleton
-    PaymentQueryBus queryBus() {
-        Map<Class<? extends Query>, QueryHandler<? extends Query, ?>> queryMap = new HashMap<>();
-
-        return new DefaultPaymentQueryBus(queryMap);
-    }
-
 }
