d3830fae18af42193cfbf8701dd26e4a5f56aa95
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-17 15:20:44 +0100

    Email class to record

--- src/main/java/org/larrieulacoste/noe/al/trademe/features/members/domain/EmailAddress.java
+++ src/main/java/org/larrieulacoste/noe/al/trademe/features/members/domain/EmailAddress.java
@@ -3,39 +3,22 @@ package org.larrieulacoste.noe.al.trademe.features.members.domain;
 import org.larrieulacoste.noe.al.trademe.domain.exception.InvalidEmailException;
 import org.larrieulacoste.noe.al.trademe.kernel.validators.StringValidators;
 
-public final class EmailAddress {
-    public final String value;
+import java.util.Objects;
+
+public record EmailAddress(String value) {
+
+    public EmailAddress {
+        Objects.requireNonNull(value);
+    }
 
     private EmailAddress(String value, StringValidators stringValidators) {
+        this(value);
         if (!stringValidators.isEmail(value)) {
             throw new InvalidEmailException("Invalid email : " + value);
         }
-        this.value = value;
     }
 
     public static EmailAddress of(String emailAddress, StringValidators stringValidators) {
         return new EmailAddress(emailAddress, stringValidators);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof EmailAddress)) return false;
-
-        EmailAddress that = (EmailAddress) o;
-
-        return value.equals(that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return "EmailAddress{" +
-                "value='" + value + '\'' +
-                '}';
     }
 }
