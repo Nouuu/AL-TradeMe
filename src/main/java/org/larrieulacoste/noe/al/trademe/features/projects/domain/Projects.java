ce43d39cbba491800f4de77d1b90b6efe24d20db
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-02-22 23:41:32 +0100

    create projects repo interface

---
+++ src/main/java/org/larrieulacoste/noe/al/trademe/features/projects/domain/Projects.java
@@ -0,0 +1,6 @@
+package org.larrieulacoste.noe.al.trademe.features.projects.domain;
+
+import org.larrieulacoste.noe.al.trademe.kernel.Repository;
+
+public interface Projects extends Repository<Project> {
+}
