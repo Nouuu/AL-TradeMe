5b5ad55cef721ef1dc3d064c16d403b0068c1673
pooetitu <pooetitu@gmail.com>
2022-03-05 15:44:53 +0100

    Add swagger requests default values

--- src/main/java/org/larrieulacoste/noe/al/trademe/features/members/web/TradesmanAbilitiesRequest.java
+++ src/main/java/org/larrieulacoste/noe/al/trademe/features/members/web/TradesmanAbilitiesRequest.java
@@ -1,14 +1,14 @@
 package org.larrieulacoste.noe.al.trademe.features.members.web;
 
-import org.larrieulacoste.noe.al.trademe.shared_kernel.model.SkillRequest;
-
 import java.util.List;
+import org.larrieulacoste.noe.al.trademe.shared_kernel.model.SkillRequest;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-record TradesmanAbilitiesRequest(String tradesmanId,
-        String profession,
+record TradesmanAbilitiesRequest(
+        String tradesmanId,
+        @Schema(defaultValue = "Plumber") String profession,
         List<SkillRequest> skills,
-        Double activityRadius,
-        Double dailyRate
+        @Schema(defaultValue = "10") Double activityRadius,
+        @Schema(defaultValue = "50") Double dailyRate
 ) {
-
 }
